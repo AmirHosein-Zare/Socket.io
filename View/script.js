3a1bbe2a4e03f2f2e999072d0a5440dc215f4c18
AmirHosein <awir.software@gmail.com>
2023-04-20 23:04:02 +0430

    add comment to front

--- View/script.js
+++ View/script.js
@@ -1,9 +1,11 @@
 const socket = io()
 
+// get data from server
 socket.on('back', (data) => {
     console.log(data.message);
 })
 
+// send data to server
 socket.emit('front', {
     message: 'salaaaaam'
 })
